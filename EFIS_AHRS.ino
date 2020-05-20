3a6faa5d43a0a3be73e3abef2c985ef343fe2ba6
Experimental Avionics <oleg@experimentalavionics.com>
2020-05-20 20:24:12 +1000

    calibration routine added

--- EFIS_AHRS.ino
+++ EFIS_AHRS.ino
@@ -1,4 +1,5 @@
 #include "Wire.h"
+#include <EEPROM.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
@@ -7,6 +8,9 @@
 
 /*********************   CAN   **************/
 const int CAN_CS_PIN = 10;   //CS pin for CAN board
+unsigned long canId;
+unsigned char len = 0;
+unsigned char ext = 0;
 
 //CAN Message details
 unsigned char canMsg[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -27,22 +31,68 @@ int AccX = 0;
 int AccY = 0;
 int AccZ = 0;
 
+
 /**********************   BNO 055   ********************************/
 
 #define BNO055_ADDRESS 0x29
-#define BNO_Mode Adafruit_BNO055::OPERATION_MODE_NDOF
+//#define BNO_Mode Adafruit_BNO055::OPERATION_MODE_NDOF
+#define BNO_Mode Adafruit_BNO055::OPERATION_MODE_NDOF_FMC_OFF
+
+/*    OPERATION_MODE_CONFIG                                   = 0X00, 
+      OPERATION_MODE_ACCONLY                                  = 0X01,
+      OPERATION_MODE_MAGONLY                                  = 0X02,
+      OPERATION_MODE_GYRONLY                                  = 0X03,
+      OPERATION_MODE_ACCMAG                                   = 0X04,
+      OPERATION_MODE_ACCGYRO                                  = 0X05,
+      OPERATION_MODE_MAGGYRO                                  = 0X06,
+      OPERATION_MODE_AMG                                      = 0X07,
+      OPERATION_MODE_IMUPLUS                                  = 0X08,
+      OPERATION_MODE_COMPASS                                  = 0X09,
+      OPERATION_MODE_M4G                                      = 0X0A,
+      OPERATION_MODE_NDOF_FMC_OFF                             = 0X0B,
+      OPERATION_MODE_NDOF                                     = 0X0C
+ */
 
 
-byte CalReg = 0;
+byte Calibration_Mode = 0;
+byte Old_Calib_Mode = 0;
+
+// BNO Calibration Variables
+byte CalMRL = 0;
+byte CalMRM = 0;
+byte CalMOXL = 0;
+byte CalMOXM = 0;
+byte CalMOYL = 0;
+byte CalMOYM = 0;
+byte CalMOZL = 0;
+byte CalMOZM = 0;
+
+int CalibrationStatus = 0;
+
+unsigned long int CalUpdateTimer = 0;
+unsigned long int CalUpdatePeriod = 5000;
+
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, BNO055_ADDRESS);
 
-uint8_t calstat; // calibration status
-String CalDataOldString =""; // Calibration data from EEPROM
+byte calstat; // calibration status
+
+/**********************  EEPROM  ***************************************/
+int BNO_MemOffset = 0;  // starting point of calibration data stored in EEPROM
+
 
 MCP_CAN CAN(CAN_CS_PIN);   // Set CS pin for CAN board
 
+#define GreenLED 5
+#define RedLED 4
+#define ButtonPin 7
+
+long unsigned int ButtonTimer = 0;
+
 void setup() {
+
+// Read Gyro calibration data from EEPROM (if available)
+    ReadGyroCalibration();
 
     Wire.begin();
 
@@ -56,13 +106,12 @@ void setup() {
     }
     CAN.setMode(MCP_NORMAL);
     Serial.println("CAN BUS Shield init ok!");
-    
-  pinMode(4, OUTPUT); 
-  pinMode(5, OUTPUT); 
-  pinMode(7, INPUT);
 
-      
-
+//Set modes for calibration button pin and status LED pins     
+ 
+    pinMode(GreenLED, OUTPUT); 
+    pinMode(RedLED, OUTPUT); 
+    pinMode(ButtonPin, INPUT);
 
     bno.begin();
 
@@ -80,112 +129,168 @@ void setup() {
   delay(50);
   writeByte(BNO055_ADDRESS, Adafruit_BNO055::BNO055_AXIS_MAP_SIGN_ADDR, 0x02);
   delay(50);
+  bno.setExtCrystalUse(true);
+  delay(50);
+  bno.setMode(BNO_Mode);
+  delay(50);
+  
+  ReadGyroCalibration(); // Read calibration data from EEPROM
 
-     CalDataOldString ="";
-/*
-     EEPROM.get(BNO_MemOffset, CalReg);
-//     CalDataOldString += String(CalReg,HEX);
-     bno.setCalvalMRL(CalReg);                    
-
-     EEPROM.get(BNO_MemOffset + 1, CalReg);
-//     CalDataOldString += String(CalReg,HEX);
-     bno.setCalvalMRM(CalReg);
-
-     EEPROM.get(BNO_MemOffset + 2, CalReg);
-     CalDataOldString += String(CalReg,HEX);
-     CalDataOldString += ":";
-     bno.setCalvalMOXL(CalReg);
-
-     EEPROM.get(BNO_MemOffset + 3, CalReg);
-     CalDataOldString += String(CalReg,HEX);
-     CalDataOldString += ":";
-     bno.setCalvalMOXM(CalReg);
-     
-     EEPROM.get(BNO_MemOffset + 4, CalReg);
-     CalDataOldString += String(CalReg,HEX);
-     CalDataOldString += ":";
-     bno.setCalvalMOYL(CalReg);
-
-     EEPROM.get(BNO_MemOffset + 5, CalReg);
-     CalDataOldString += String(CalReg,HEX);
-     CalDataOldString += ":";
-     bno.setCalvalMOYM(CalReg);
-
-     EEPROM.get(BNO_MemOffset + 6, CalReg);
-     CalDataOldString += String(CalReg,HEX);
-     CalDataOldString += ":";
-     bno.setCalvalMOZL(CalReg);
-
-     EEPROM.get(BNO_MemOffset + 7, CalReg);
-     CalDataOldString += String(CalReg,HEX);
-     bno.setCalvalMOZM(CalReg);
-     
-    
-//      Serial.println(CalDataOldString);
-*/
-      bno.setMode(Adafruit_BNO055::OPERATION_MODE_NDOF);
-//      bno.setMode(Adafruit_BNO055::OPERATION_MODE_NDOF_FMC_OFF);
-      delay(50);
-
-       bno.setExtCrystalUse(true);
-       delay(50);
-
-
-
-  //     digitalWrite(4, HIGH);
-  //     digitalWrite(5, HIGH);
+  // Just in case check if the it is actually some meaningfull data, not just empty bytes (0xFF)
+  if (CalMRL == 0xFF and CalMRM == 0xFF) {
+    Serial.println("###### EEPROM is empty. Caibrate the Gyro ######");
+  } else {
+    UpdateGyroCalibration();
+  }
 
 }
 
 void loop() {
- // digitalWrite(4, digitalRead(7));
+// Read messages from CAN network
+if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
+  {
+    CAN.readMsgBuf(&canId, &ext, &len, canMsg);    // read data,  len: data length, buf: data buf
+    switch (canId) {
+      case 35:
+        {
+        // First byte [0] is calibration command
+        Serial.println("Message 35 arrived" + String(canMsg[0], HEX));
+        // FIRST bit in the [0] byte is a signal to save the data into EEPROM
+        // make sure the save command is not executed too often. EEPROM has limited number of write cycles (100000)
+          if (canMsg[0] == 1) {
+            Serial.println("writing calibration");
+            WriteGyroCalibration();
+            // flash the Red LED for a few moments (if installed)
+            for (int i = 0; i <= 10; i++) {
+              digitalWrite(RedLED, !digitalRead(RedLED));
+              delay(200);
+            }
+          }
+          
+        // SECOND bit in the [0] byte is a signal to temporary disable the calibration updates 
+        // to ensure they dont messup calibration efforts
+          if (canMsg[0] == 0x02) {
+            Calibration_Mode = 1;
+            Serial.println("pause calibration refresh ");
+            digitalWrite(RedLED, 1);
+          } else {
+            Calibration_Mode = 0;
+            Serial.println("resume calibration refresh");
+            digitalWrite(RedLED, 0);
+          }
+        }
+        break;
+      default: 
+        // if nothing else matches, do the default
+        // default is optional
+      break;
+    }
+  }
+  
+// BEGIN of the Button and LED handling block *********************************************************
+// the following block if for debugging only.
+// it controlls the onboard button and the LED's
+// calibration can be saved into EEPROM by activating the "calibration" mode by pressing the button once
+// Red LED should be steady ON
+// once satisfied with the calibration status press and hold the button untill the Red LED start slashing
+// at that point the calibration will be written to EEPROM and temporary variables.
+ 
+  if (digitalRead(ButtonPin) == 0) {
+    digitalWrite(GreenLED,1);
+    if (ButtonTimer == 0) {
+      ButtonTimer = millis();
+    } else {
+      if (millis() - ButtonTimer > 2000 and digitalRead(RedLED) == 1) {
+        digitalWrite(RedLED,1);
+        // write calibration data into EEPROM
+        for (int i = 0; i <= 10; i++) {
+           digitalWrite(RedLED, !digitalRead(RedLED));
+           delay(200);
+        }
+        WriteGyroCalibration();
+        Calibration_Mode = 0;
+        digitalWrite(RedLED,0);
+        ButtonTimer = 0;
+      }
+    }
+  } else {
+    digitalWrite(GreenLED,0);
+    
+    //toggle Red Led and calibration status
+    if (ButtonTimer > 0) { //button released
+      digitalWrite(RedLED, !digitalRead(RedLED));
+      if (Calibration_Mode == 0) {
+        Calibration_Mode = 1;
+      } else {
+        Calibration_Mode = 0;
+      }
+    }
+    
+    ButtonTimer = 0;
+    
+  }
+  
+  if (Calibration_Mode == 1) {
+    Gyro_Calibration();
+  } 
+  
+// END of the Button and LED handling block *********************************************************
 
- Gyro();
+  Gyro();
 
 // Send Euler data
-if (millis() > CAN_Euler_Timestamp + CAN_Euler_Period + random(0, 10)) {
-
-  canMsg[0] = MAG;
-  canMsg[1] = MAG >> 8;
-
-  canMsg[2] = gRoll;
-  canMsg[3] = gRoll >> 8;
-
-  canMsg[4] = gPitch;
-  canMsg[5] = gPitch >> 8;
-
-  canMsg[6] = gTurnRate;
-  canMsg[7] = gTurnRate >> 8;
-  
-
-  CAN.sendMsgBuf(CAN_Euler_Msg_ID, 0, 8, canMsg); 
-  
-  CAN_Euler_Timestamp = millis();
-}
+    if (millis() > CAN_Euler_Timestamp + CAN_Euler_Period + random(0, 10)) {
+    
+      canMsg[0] = MAG;
+      canMsg[1] = MAG >> 8;
+    
+      canMsg[2] = gRoll;
+      canMsg[3] = gRoll >> 8;
+    
+      canMsg[4] = gPitch;
+      canMsg[5] = gPitch >> 8;
+    
+      canMsg[6] = gTurnRate;
+      canMsg[7] = gTurnRate >> 8;
+      
+    
+      CAN.sendMsgBuf(CAN_Euler_Msg_ID, 0, 8, canMsg); 
+      
+      CAN_Euler_Timestamp = millis();
+    }
 
 // Send Acceleration data
-if (millis() > CAN_Acc_Timestamp + CAN_Acc_Period + random(0, 10)) {
+    if (millis() > CAN_Acc_Timestamp + CAN_Acc_Period + random(0, 10)) {
+    
+      canMsg[0] = AccX;
+      canMsg[1] = AccX >> 8;
+    
+      canMsg[2] = AccY;
+      canMsg[3] = AccY >> 8;
+    
+      canMsg[4] = AccZ;
+      canMsg[5] = AccZ >> 8;
 
-  canMsg[0] = AccX;
-  canMsg[1] = AccX >> 8;
-
-  canMsg[2] = AccY;
-  canMsg[3] = AccY >> 8;
-
-  canMsg[4] = AccZ;
-  canMsg[5] = AccZ >> 8;
-
-  CAN.sendMsgBuf(CAN_Acc_Msg_ID, 0, 6, canMsg); 
-  
-  CAN_Acc_Timestamp = millis();
-}
+      canMsg[6] = calstat;
+    
+      CAN.sendMsgBuf(CAN_Acc_Msg_ID, 0, 7, canMsg); 
+      
+      CAN_Acc_Timestamp = millis();
+    }
 
  
+// Update calibration
+// If calibration mode is activated this update will not run to avoid messing up the calibration effors
+  if (millis()-CalUpdateTimer > CalUpdatePeriod and Calibration_Mode == 0) {
+    UpdateGyroCalibration();
+    CalUpdateTimer = millis();
+  }
+
 
 }
 
 
- uint8_t readByte(uint8_t address, uint8_t subAddress)
+uint8_t readByte(uint8_t address, uint8_t subAddress)
 {
  uint8_t data; // `data` will store the register data   
   Wire.beginTransmission(address);         // Initialize the Tx buffer
@@ -217,4 +322,3 @@ void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * des
   while (Wire.available()) {
         dest[i++] = Wire.read(); }         // Put read results in the Rx buffer
 }
-
--- Gyro.ino
+++ Gyro.ino
@@ -1,68 +1,4 @@
 void Gyro() {
-
-
-  // Check calibration status of the sensors
-
-  uint8_t calstat = readByte(0x29, Adafruit_BNO055::BNO055_CALIB_STAT_ADDR);
-//  Serial.println("Not calibrated = 0, fully calibrated = 3");
-//  Serial.print("System calibration status "); Serial.println( (0xC0 & calstat) >> 6);
-//  Serial.print("Gyro   calibration status "); Serial.println( (0x30 & calstat) >> 4);
-//  Serial.print("Accel  calibration status "); Serial.println( (0x0C & calstat) >> 2);
-//  Serial.print("Mag    calibration status "); Serial.println( (0x03 & calstat) >> 0);
-
-  /*
-  bno.setMode(Adafruit_BNO055::OPERATION_MODE_CONFIG);
-  Serial.println(bno.getCalvalMOXL());
-  Serial.println(bno.getCalvalMOXM());
-  Serial.println(bno.getCalvalMOYL());
-  Serial.println(bno.getCalvalMOYM());
-  Serial.println(bno.getCalvalMOZL());
-  Serial.println(bno.getCalvalMOZM());
-  bno.setMode(Adafruit_BNO055::OPERATION_MODE_NDOF);
-  */
-  calstat = bno.getCalib();
-  Serial.println(0x03 & calstat);
-  
-  sensors_event_t event; 
-  bno.getEvent(&event);
-
-  MAG = event.orientation.x;
-  gRoll = (-1)*event.orientation.y;
-  gPitch = (-1)*event.orientation.z;
-
- Serial.print("Pitch = ");
-Serial.println(gPitch);
-
-Serial.print("Roll = ");
-Serial.println(gRoll);
-
-Serial.print("Heading = ");
-Serial.println(MAG);
-
-/*  
-  if (abs(gPitch)>90) {
-    if (gRoll > 0 ) {
-      gRoll = 180-gRoll;
-    } else {
-      gRoll = -180-gRoll;
-    }
-  }
-
-  if (gPitch>90) {
-    gPitch = 180 - gPitch;
-  }
-
-  if (gPitch<-90) {
-    gPitch = -180 - gPitch;
-  }
-*/
-
-
-
-
-
-
-//************************ Slip ball **************************
 /*
 VECTOR_MAGNETOMETER (values in uT, micro Teslas)
 VECTOR_GYROSCOPE (values in rps, radians per second)
@@ -70,22 +6,127 @@ VECTOR_EULER (values in Euler angles or 'degrees', from 0..359)
 VECTOR_ACCELEROMETER (values in m/s^2)
 VECTOR_LINEARACCEL (values in m/s^2)
 VECTOR_GRAVITY (values in m/s^2)
-*/
-  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+*/ 
 
-Serial.print("X: ");
-Serial.print(acc.x());
-Serial.print(" Y: ");
-Serial.print(acc.y());
-Serial.print(" Z: ");
-Serial.print(acc.z());
-Serial.println("");
+// Get EULER data
+
+  sensors_event_t event; 
+  bno.getEvent(&event);
+
+  MAG = event.orientation.x;
+  gRoll = (-1)*event.orientation.y;
+  gPitch = (-1)*event.orientation.z;
+
+
+//Get acceleration data
+
+  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
 
   AccX = acc.x()*100; //slip ball
   AccY = acc.y()*100; //forward acceleration
   AccZ = acc.z()*100; //wing loading
 
-
+  calstat = bno.getCalib();
+  
 }
 
+void ReadGyroCalibration() {
+     EEPROM.get(BNO_MemOffset, CalMRL);
+     EEPROM.get(BNO_MemOffset + 1, CalMRM);
+     EEPROM.get(BNO_MemOffset + 2, CalMOXL);
+     EEPROM.get(BNO_MemOffset + 3, CalMOXM);
+     EEPROM.get(BNO_MemOffset + 4, CalMOYL);
+     EEPROM.get(BNO_MemOffset + 5, CalMOYM);
+     EEPROM.get(BNO_MemOffset + 6, CalMOZL);
+     EEPROM.get(BNO_MemOffset + 7, CalMOZM);
+}
 
+void WriteGyroCalibration() {
+// if satisfied with calibration status, save the calibration data into EEPROM so it could be written later into BNO0055 on startup 
+// or when BNO's self-calibration messes up the data
+
+    Serial.println("###### Writing calibration to EEPROM ######");
+    bno.setMode(Adafruit_BNO055::OPERATION_MODE_CONFIG);
+    delay(50);
+     
+     // Read calibration data from the sensor
+    CalMRL = bno.getCalvalMRL(); 
+    CalMRM = bno.getCalvalMRM();
+    CalMOXL = bno.getCalvalMOXL();
+    CalMOXM = bno.getCalvalMOXM();
+    CalMOYL = bno.getCalvalMOYL();
+    CalMOYM = bno.getCalvalMOYM();
+    CalMOZL = bno.getCalvalMOZL();
+    CalMOZM = bno.getCalvalMOZM();
+
+    EEPROM.put(BNO_MemOffset, CalMRL);
+    EEPROM.put(BNO_MemOffset + 1, CalMRM);
+    EEPROM.put(BNO_MemOffset + 2, CalMOXL);
+    EEPROM.put(BNO_MemOffset + 3, CalMOXM);
+    EEPROM.put(BNO_MemOffset + 4, CalMOYL);
+    EEPROM.put(BNO_MemOffset + 5, CalMOYM);
+    EEPROM.put(BNO_MemOffset + 6, CalMOZL);
+    EEPROM.put(BNO_MemOffset + 7, CalMOZM);
+
+    bno.setMode(BNO_Mode);
+    delay(50);
+}
+
+void Gyro_Calibration() {
+  // this procedure is just for bebugging only
+  // it is controlled via on-board button
+  // It shows calibration status in the serial output. 
+  // There are 4 of them - System, Gyro, Accelerometer and Magnetometer
+  // Best calibration if the values for all four equal to 3.
+
+  //  uint8_t calstat = readByte(0x29, Adafruit_BNO055::BNO055_CALIB_STAT_ADDR);
+
+  calstat = bno.getCalib();
+
+  byte calSys = (0xC0 & calstat) >> 6;
+  byte calGyro = (0x30 & calstat) >> 4;
+  byte calAccel = (0x0C & calstat) >> 2;
+  byte calMag = (0x03 & calstat) >> 0;
+
+  Serial.println("==============================");
+  Serial.println("Sys: " + String(calSys) + " G:" + String(calGyro) + " A:" + String(calAccel) + " M:" + String(calMag));
+  
+  Serial.print("Pitch = ");
+  Serial.println(gPitch);
+
+  Serial.print("Roll = ");
+  Serial.println(gRoll);
+
+  Serial.print("Heading = ");
+  Serial.println(MAG);
+
+  Serial.println("Acceleration");
+  Serial.print("X: ");
+  Serial.print(AccX);
+  Serial.print(" Y: ");
+  Serial.print(AccY);
+  Serial.print(" Z: ");
+  Serial.print(AccZ);
+  Serial.println("");
+
+  delay(100);
+}
+
+void UpdateGyroCalibration() {
+  // write calibration data into BNO055 chip
+  Serial.println("=****= Updating Calibration =****=");
+  bno.setMode(Adafruit_BNO055::OPERATION_MODE_CONFIG);
+  delay(50);
+  bno.setCalvalMRL(CalMRL);                    
+  bno.setCalvalMRM(CalMRM);
+  bno.setCalvalMOXL(CalMOXL);
+  bno.setCalvalMOXM(CalMOXM);
+  bno.setCalvalMOYL(CalMOYL);
+  bno.setCalvalMOYM(CalMOYM);
+  bno.setCalvalMOZL(CalMOZL);
+  bno.setCalvalMOZM(CalMOZM);
+  
+  bno.setMode(BNO_Mode);
+  delay(50);
+
+}
